dbf9bffd7ee77e473adff82a4acbeba6be7666e1
rudy3091 <grayblack313@gmail.com>
2021-05-05 03:50:25 +0900

    Replace WebConfiguration.java to CorsFilter.java

---
+++ src/main/java/com/rudy/jwtloginexample/config/filters/CorsFilter.java
@@ -0,0 +1,39 @@
+package com.rudy.jwtloginexample.config.filters;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Methods","*");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers",
+                "Origin, x-access-token, X-Requested-With, Content-Type, Accept, Key, Authorization");
+
+        if (req.getMethod().equals("OPTIONS")) {
+            res.setStatus(200);
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+}
